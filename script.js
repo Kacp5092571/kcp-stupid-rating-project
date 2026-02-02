7c288be8e2294730e1ab6cd975df1f32ea1d442b
Kacp5092571 <kacperkawec481@gmail.com>
2026-02-02 17:19:53 +0000

    fix?

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function hovering1(){
 document.getElementById("img1").src ="https://kacp5092571.github.io/kcp-stupid-rating-project/bright1star.png";
 document.getElementById("p1").innerHTML = "boo";
 document.getElementById("p2").innerHTML = "boo";
-document.getElementById("p3").innerHTML = "boo";
-timeout = setTimeout(hovering1, 2000);
+document.getElementById("p3").innerHTML = "boo"
+alert('hi')
 let you=1
 while(you === 1){
     confirm('HOW DARE YOU')
