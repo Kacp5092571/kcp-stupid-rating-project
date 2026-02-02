2cf5416a251d9f7d38c9e11167c8c4cda82e9280
Kacp5092571 <kacperkawec481@gmail.com>
2026-02-02 17:08:28 +0000

    ed

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function hovering1(){
 document.getElementById("img1").src ="https://kacp5092571.github.io/kcp-stupid-rating-project/bright1star.png";
-document.getElementById("p1").style.display = "none";
-document.getElementById("p2").style.display = "none";
-document.getElementById("p3").style.display = "none";
+document.getElementById("p1").style.display = "boo";
+document.getElementById("p2").innerHTML = "boo";
+document.getElementById("p3").innerHTML = "boo";
 let you=1
 while(you === 1){
     confirm('HOW DARE YOU')
