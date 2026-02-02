a3e8b1576f672d80b6a00aec8ff2dadb85b965d5
Kacp5092571 <kacperkawec481@gmail.com>
2026-02-02 17:16:52 +0000

    fix???

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ document.getElementById("img1").src ="https://kacp5092571.github.io/kcp-stupid-r
 document.getElementById("p1").innerHTML = "boo";
 document.getElementById("p2").innerHTML = "boo";
 document.getElementById("p3").innerHTML = "boo";
+timeout = setTimeout(hovering1, 2000);
+let you=1
+while(you === 1){
+    confirm('HOW DARE YOU')
+}
 }
