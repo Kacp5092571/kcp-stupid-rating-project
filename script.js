ba7ce617eca6dd787c512ad9c0b789418badd9c1
Kacp5092571 <kacperkawec481@gmail.com>
2026-02-02 17:09:03 +0000

    d

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function hovering1(){
 document.getElementById("img1").src ="https://kacp5092571.github.io/kcp-stupid-rating-project/bright1star.png";
-document.getElementById("p1").style.display = "boo";
+document.getElementById("p1").innerHTML = "boo";
 document.getElementById("p2").innerHTML = "boo";
 document.getElementById("p3").innerHTML = "boo";
 let you=1
