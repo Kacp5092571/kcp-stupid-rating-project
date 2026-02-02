73ebaf730463a2c99aacc0b465baf8f06978771f
Kacp5092571 <kacperkawec481@gmail.com>
2026-02-02 17:11:15 +0000

    xxx

--- script.js
+++ script.js
@@ -3,9 +3,4 @@ document.getElementById("img1").src ="https://kacp5092571.github.io/kcp-stupid-r
 document.getElementById("p1").innerHTML = "boo";
 document.getElementById("p2").innerHTML = "boo";
 document.getElementById("p3").innerHTML = "boo";
-let you=1
-while(you === 1){
-    confirm('HOW DARE YOU')
-let you = you-1
-}
 }
